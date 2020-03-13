e99dc1ce812970946b089df266529a4314fbccbf
Benjamin S Ogzewalla <saigeogzewalla@gmail.com>
2020-03-13 05:13:44 -0400

    Background Update

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -39,8 +39,7 @@ function App() {
 
 
   return (
-    <div className={
-      (typeof weather.main != "undefined") ? ((weather.main.temp) ? 'app warm' : 'app') : 'app'}>
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app-warm' : 'app') : 'app'}>
      <main>
 
        <div className="search-box">
