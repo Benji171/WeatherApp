8b90e23310b8c7fa86b9b646026944709c388087
Benjamin S Ogzewalla <saigeogzewalla@gmail.com>
2020-03-13 05:06:08 -0400

    Changing Backgrounds

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -39,7 +39,8 @@ function App() {
 
 
   return (
-    <div className="app">
+    <div className={
+      (typeof weather.main != "undefined") ? ((weather.main.temp) ? 'app warm' : app) : 'app'}>
      <main>
 
        <div className="search-box">
