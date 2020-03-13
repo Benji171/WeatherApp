7f4cbc354fc8ac8aa9d0f1e5059b082cfbbd971a
Benjamin S Ogzewalla <saigeogzewalla@gmail.com>
2020-03-13 04:55:57 -0400

    Update Api Call

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 const api = {
   key: '4f202515b8b40e20a36d3096794eeaa3',
-  base: 'api.openweathermap.org/data/2.5/'
+  base: 'https://api.openweathermap.org/data/2.5/'
 }
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
 
   const search = e => {
     if (e.key === "Enter") {
-      fetch(`${api.base}weather?q=${query}&units=metric%APPID=${api.key}`)
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
       .then(res => res.json())
       .then(result => {
         console.log(result);
@@ -56,13 +56,15 @@ function App() {
        {(typeof weather.main != "undefined") ? (
          <div>
             <div className="location-box">
-              <div className="location">{weather.name}, {weather.sys.country}}</div>
+              <div className="location">{weather.name}, {weather.sys.country}</div>
               <div className="date">{dateBuilder(new Date())}</div>
             </div>
 
             <div className="weather-box">
-              <div className="temp">15</div>
-              <div className="weather">Sunny</div>
+              <div className="temp">
+                {Math.round(weather.main.temp)}
+              </div>
+              <div className="weather">{weather.weather}}</div>
             </div>
          </div>
        ) : ("")}
