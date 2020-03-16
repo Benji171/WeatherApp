725b25e251a4d40039e07eea827f0ca6a1b24580
Benjamin S Ogzewalla <saigeogzewalla@gmail.com>
2020-03-16 15:17:43 -0400

    Updates

---
+++ precache-manifest.6a7d0fbcc8372be40a3904e2df7aa656.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53e791e8c1d8c6255c3c22edb204d6b6",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "c3a402fb71b49e9d7045",
+    "url": "/WeatherApp/static/css/main.c86a921d.chunk.css"
+  },
+  {
+    "revision": "d223694086e819680b7b",
+    "url": "/WeatherApp/static/js/2.c25be8e0.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/WeatherApp/static/js/2.c25be8e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3a402fb71b49e9d7045",
+    "url": "/WeatherApp/static/js/main.c4798311.chunk.js"
+  },
+  {
+    "revision": "d6d765c39ca50d6c9325",
+    "url": "/WeatherApp/static/js/runtime-main.db864e50.js"
+  },
+  {
+    "revision": "7d92fbb1ce6b5b1d5b94a26d00cdb120",
+    "url": "/WeatherApp/static/media/clear-bg.7d92fbb1.jpg"
+  },
+  {
+    "revision": "7a754bca97ccfff5fcdb8567d98dc11c",
+    "url": "/WeatherApp/static/media/clouds-bg.7a754bca.jpg"
+  },
+  {
+    "revision": "09cb58fe80fda6b7be23e4ffd235dd02",
+    "url": "/WeatherApp/static/media/rain-bg.09cb58fe.jpg"
+  },
+  {
+    "revision": "74393a441db5e87dcd552b103444ab93",
+    "url": "/WeatherApp/static/media/thunderstorm-bg.74393a44.jpg"
+  }
+]);
